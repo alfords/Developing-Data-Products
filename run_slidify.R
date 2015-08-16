b916426b886bb9070390e6739aec49dd72ae8ca9
al <aaron_zhang@ymail.com>
2015-08-16 21:14:59 +0800

    Create run_slidify.R

---
+++ run_slidify.R
@@ -0,0 +1,16 @@
+#install.packages("devtools")
+require(devtools)
+#install_github('slidify','ramnathv')
+#install_github('slidifyLibraries','ramnathv')
+library(slidify)
+#install_github('rCharts', 'ramnathv')
+#install_github('shiny', 'rstudio')
+
+getwd()
+#setwd("..")
+setwd("developing-data-products")
+
+#author("alfords") 
+slidify("index.Rmd")
+browseURL("index.html")
+publish(username = "alfords", repo = "developing-data-products")
